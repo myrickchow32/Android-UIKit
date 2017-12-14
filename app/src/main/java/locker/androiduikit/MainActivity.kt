e953bef7eeb172131135b969670b711e5e5bb2e5
Myrick Chow <myrick@redso.com.hk>
2017-12-14 21:18:54 +0800

    Init commit

---
+++ app/src/main/java/locker/androiduikit/MainActivity.kt
@@ -0,0 +1,12 @@
+package locker.androiduikit
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
